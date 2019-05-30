9fcee5f186433037b62bf0526eebc137e4875244
Whltrosed <317471341@qq.com>
2019-05-30 22:08:36 +0800

    结尾区域

--- public/index.js
+++ public/index.js
@@ -53,3 +53,15 @@ new Hu({
     }();
   }
 });
+
+// Footer
+new Hu({
+  el: '#app-footer',
+  render( html ){
+    return html`
+      <hr>
+      <div class="title">社会主义核心价值观：富强、民主、文明、和谐、自由、平等、公正、法治、爱国、敬业、诚信、友善</div>
+      <br>
+    `;
+  }
+});
