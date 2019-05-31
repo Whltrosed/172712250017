3de9e438ee1f6059b0d3e9eea6cc7f689f171296
Whltrosed <317471341@qq.com>
2019-05-31 13:45:17 +0800

    更新数据

--- public/data.js
+++ public/data.js
@@ -12,37 +12,39 @@
     db.transaction(( tx ) => {
       // 创建工程建设数据库
       tx.executeSql(`create table gcjs( name, content, time, state )`);
-      // 插入工程建设数据
-      tx.executeSql(`insert into gcjs ( name, content, time, state ) values ( 'name - 1', 'content - 1', '2019-05-30', 0 )`);
-      tx.executeSql(`insert into gcjs ( name, content, time, state ) values ( 'name - 2', 'content - 2', '2019-05-29', 0 )`);
-      tx.executeSql(`insert into gcjs ( name, content, time, state ) values ( 'name - 3', 'content - 3', '2019-05-28', 0 )`);
-      tx.executeSql(`insert into gcjs ( name, content, time, state ) values ( 'name - 4', 'content - 4', '2019-05-27', 0 )`);
-      tx.executeSql(`insert into gcjs ( name, content, time, state ) values ( 'name - 5', 'content - 5', '2019-05-26', 1 )`);
-      tx.executeSql(`insert into gcjs ( name, content, time, state ) values ( 'name - 6', 'content - 6', '2019-05-25', 1 )`);
-      tx.executeSql(`insert into gcjs ( name, content, time, state ) values ( 'name - 7', 'content - 7', '2019-05-24', 1 )`);
-      tx.executeSql(`insert into gcjs ( name, content, time, state ) values ( 'name - 8', 'content - 8', '2019-05-23', 1 )`);
-      tx.executeSql(`insert into gcjs ( name, content, time, state ) values ( 'name - 9', 'content - 9', '2019-05-22', 1 )`);
-      tx.executeSql(`insert into gcjs ( name, content, time, state ) values ( 'name - 10', 'content - 10', '2019-05-21', 1 )`);
-      tx.executeSql(`insert into gcjs ( name, content, time, state ) values ( 'name - 11', 'content - 11', '2019-05-20', 1 )`);
-      tx.executeSql(`insert into gcjs ( name, content, time, state ) values ( 'name - 12', 'content - 12', '2019-05-19', 1 )`);
-      tx.executeSql(`insert into gcjs ( name, content, time, state ) values ( 'name - 13', 'content - 13', '2019-05-18', 1 )`);
-
       // 创建政府采购数据库
       tx.executeSql(`create table zfcg( name, content, time, state )`);
+
+      // 插入工程建设数据
+      tx.executeSql(`insert into gcjs ( name, content, time, state ) values ( '某某市人民法院审判法庭工程, 招标编号：GC20190003', '${
+`
+  1.工程名称: 某某市人民法院审判法庭工程
+  2.项目审批机关名称某某市发展和改革委员会
+  3.招标人: 某某市人民法院
+  4.资金来源: 财政
+  5.交易项目性质: 建筑工程
+`
+      }', '2019/05/09', 0 )`);
+      tx.executeSql(`insert into gcjs ( name, content, time, state ) values ( '某某县看守所武警中队营房建设项目, 招标编号：GC20190156', '${
+`
+  1.工程名称：某某县看守所武警中队营房建设项目
+  2.项目审批机关名称：某某县发展和改革委员会
+  3.招标人：某某县公安局
+  4.资金来源：财政资金
+  5.项目交易性质:房屋建筑业
+`
+      }', '2019/4/23', 0 )`);
       // 插入政府采购数据
-      tx.executeSql(`insert into zfcg ( name, content, time, state ) values ( '公开招标：name - 1', 'content - 1', '2019-04-30', 0 )`);
-      tx.executeSql(`insert into zfcg ( name, content, time, state ) values ( '公开招标：name - 2', 'content - 2', '2019-04-29', 0 )`);
-      tx.executeSql(`insert into zfcg ( name, content, time, state ) values ( '公开招标：name - 3', 'content - 3', '2019-04-28', 0 )`);
-      tx.executeSql(`insert into zfcg ( name, content, time, state ) values ( '公开招标：name - 4', 'content - 4', '2019-04-27', 0 )`);
-      tx.executeSql(`insert into zfcg ( name, content, time, state ) values ( '公开招标：name - 5', 'content - 5', '2019-04-26', 0 )`);
-      tx.executeSql(`insert into zfcg ( name, content, time, state ) values ( '公开招标：name - 6', 'content - 6', '2019-04-25', 0 )`);
-      tx.executeSql(`insert into zfcg ( name, content, time, state ) values ( '公开招标：name - 7', 'content - 7', '2019-04-24', 1 )`);
-      tx.executeSql(`insert into zfcg ( name, content, time, state ) values ( '公开招标：name - 8', 'content - 8', '2019-04-23', 1 )`);
-      tx.executeSql(`insert into zfcg ( name, content, time, state ) values ( '公开招标：name - 9', 'content - 9', '2019-04-22', 1 )`);
-      tx.executeSql(`insert into zfcg ( name, content, time, state ) values ( '公开招标：name - 10', 'content - 10', '2019-04-21', 1 )`);
-      tx.executeSql(`insert into zfcg ( name, content, time, state ) values ( '公开招标：name - 11', 'content - 11', '2019-04-20', 1 )`);
-      tx.executeSql(`insert into zfcg ( name, content, time, state ) values ( '公开招标：name - 12', 'content - 12', '2019-04-19', 1 )`);
-      tx.executeSql(`insert into zfcg ( name, content, time, state ) values ( '公开招标：name - 13', 'content - 13', '2019-04-18', 1 )`);
+      tx.executeSql(`insert into zfcg ( name, content, time, state ) values ( '公开招标：某某市某社区服务中心老旧小区基础性物业管理服务, 项目编号：2019CGF021', '${
+`
+  按法定授权，某某市公共资源交易中心受采购人某社区公共服务中心的委托，现对某某市某社区公共服务中心老旧小区基础性物业管理服务项目项目进行公开招标，欢迎具备条件的国内投标供应商参加投标。
+`
+      }', '2019/05/26', 0 )`);
+      tx.executeSql(`insert into zfcg ( name, content, time, state ) values ( '公开招标：2019年某大学多媒体教室设备项目, 项目编号：2019CGH026', '${
+`
+  按法定授权，某某市公共资源交易中心受采购人某大学的委托，现对2019年某大学多媒体教室设备项目进行公开招标，欢迎具备条件的国内投标供应商参加投标。
+`
+      }', '2019/04/22', 0 )`);
 
       // 标识已初始化数据
       localStorage.setItem( 'isInit', 'true' );
